b74f98b09cc2b9388ec9230988c967a3e9b79745
NikithGanga <nikithganga123@gmail.com>
2024-12-07 18:13:36 +0530

    Fixes and aPP.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'tle.nikith123@example.com'  # Your email address
 app.config['MAIL_PASSWORD'] = 'Nikith@123'          # Your email password
